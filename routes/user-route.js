a9fa5aed296e0948352256873b6e3e78a4526b49
InacioMugana <inaciomugaua@gmail.com>
2024-07-26 12:48:05 +0200

    first commit

---
+++ routes/user-route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getAllUser,
+  getUser,
+  signup,
+  login,
+} = require("../controllers/user-controller");
+
+router.get("/", getAllUser);
+
+router.get("/:id", getUser);
+
+router.post("/", signup);
+
+router.post("/", login);
+
+module.exports = router;
